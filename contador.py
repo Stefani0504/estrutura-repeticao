0a5463b42bde7ee6ff2d2d765dfe27255813ede7
stefanisilva <stefani.arsilva@senacsp.edu.br>
2026-02-05 21:34:38 -0300

    contador com while

---
+++ contador.py
@@ -0,0 +1,46 @@
+'''
+
+o que é uma estrutura de repetição?
+
+-> Serve para executar um bloco de código várias vezes
+-> ENQUANTO UMA CONDIÇÃO FOR VERDADEIRA
+
+'''
+variavel = True #Boolean
+
+contador = 0
+
+while contador <= 10:
+    contador = contador + 1
+    print(contador)   contador = contador + 1
+
+# quero fazer uma contagem de 10 até 0 (DECRESCENTE)
+
+contador = 10
+
+while contador >= 0:
+    print(contador)
+    contador = contador -1
+
+
+
+    '''
+    Qual a lógica de uma tabuada? Quero fazer a tabuada do 5 e deve mostrar
+
+
+'''
+    # Tabuada do 5
+
+    tabuada = 5
+    contador = 0
+
+    while contador <= 10:
+        print(tabuada, "x",  contador, "=", contador * tabuada) 
+        contador += 1
+    
+    
+
+
+
+
+   
